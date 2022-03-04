88d4674b892e678072abee32f896dc994afce6bf
unknown <contato.marcoscandido@gmail.com>
2022-03-04 16:48:04 -0300

    Atualizações de postagem

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import {HttpClientModule} from '@angular/common/http';
 import { FormsModule } from '@angular/forms'; 
 import { AppRoutingModule } from './app-routing.module';
+import{HashLocationStrategy,LocationStrategy} from '@angular/common'
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { RodapeComponent } from './rodape/rodape.component';
 import { EntrarComponent } from './entrar/entrar.component';
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
 import { InicioComponent } from './inicio/inicio.component';
+
 
 
 @NgModule({
@@ -27,7 +29,11 @@ import { InicioComponent } from './inicio/inicio.component';
     FormsModule
 
   ],
-  providers: [],
+  providers: [{
+
+    provide : LocationStrategy,
+     useClass: HashLocationStrategy
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
