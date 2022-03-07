8ac128de1f5da556fc29171b8923e286d3c8f0da
unknown <contato.marcoscandido@gmail.com>
2022-03-06 21:02:41 -0300

    put-delete-tema em andamento

--- src/app/service/tema.service.ts
+++ src/app/service/tema.service.ts
@@ -1,5 +1,5 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { Injectable } from '@angular/core'; 
 import { Observable, TeardownLogic } from 'rxjs';
 import { environment } from 'src/environments/environment.prod';
 import { Tema } from '../model/Tema';
@@ -21,7 +21,22 @@ export class TemaService {
 
   }
 
+  getByIdTema (id : number):Observable<Tema>{
+    
+    return this.http.get<Tema>(`http://localhost:8080/tema/${id}`,this.token)
+    
+  }
+
   postTema(tema:Tema): Observable<Tema>{
     return this.http.post<Tema>('http://localhost:8080/temas',tema,this.token)
+  }
+
+  putTema(tema: Tema): Observable<Tema>{
+    return this.http.put<Tema>('http://localhost:8080/temas',tema,this.token)
+  }
+
+  deleteTema(id:number){
+
+    return this.http.delete(`http://localhost:8080/temas/${id}`,this.token)
   }
 }
